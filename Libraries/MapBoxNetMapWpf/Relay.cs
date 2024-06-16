abb2eb564d1cc600faac386093ba78be598c10ef
Angus Oliver - Fraise <angus.oliver@proemion.com>
2024-06-16 20:55:13 +0200

    Tying to add new libraries

---
+++ Libraries/MapBoxNetMapWpf/Relay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace MapboxNetWPF
+{
+    class Relay
+    {
+        Dispatcher dispatcher;
+        Action<string> callback;
+
+        public Relay(Action<string> callback, Dispatcher dispatcher)
+        {
+            this.dispatcher = dispatcher;
+            this.callback = callback;
+        }
+
+        public void notify(string json)
+        {
+            dispatcher.BeginInvoke((Action)(() =>
+            {
+                callback(json);
+            }));
+        }
+    }
+}
